5731c279d7557a0c95877fd512c9d29170e1d8a4
Photonic0 <62021912+Photonic0@users.noreply.github.com>
2023-05-28 18:01:17 -0300

    hopefuly bow is done and I didn't fuck up anything

--- Content/Foresta/Items/Weapons/Ranged/Crusolium/CrusoliumBowHeldProj.cs
+++ Content/Foresta/Items/Weapons/Ranged/Crusolium/CrusoliumBowHeldProj.cs
@@ -28,11 +28,13 @@ namespace Crystals.Content.Foresta.Items.Weapons.Ranged.Crusolium
         {
             Projectile.friendly = true;
             Projectile.penetrate = -1;
-            Projectile.Size = new Vector2(10);
+            Projectile.Size = new Vector2(1);
         }
         public float MaxTimer { get => Projectile.ai[2]; set => Projectile.ai[2] = value; }//MIGHT NOT NEED THIS ACTUALLY
         public float ChargeAmount { get => Projectile.ai[1]; set => Projectile.ai[1] = value; }
         public float MaxCharge { get => Projectile.ai[0]; }
+        public float ChargeProgress { get => Projectile.ai[1] / Projectile.ai[0]; }
+        public float ChargeProgressWithEasing { get => (float)Helpers.EaseFunctions.EaseOutBack(Projectile.ai[1] / Projectile.ai[0]); }
         public override void AI()
         {
             //UNFINISHED
@@ -44,27 +46,36 @@ namespace Crystals.Content.Foresta.Items.Weapons.Ranged.Crusolium
             if (!player.channel && ChargeAmount >= MaxCharge)
             {
                 ChargeAmount = 0;
-                int ammoIdToShoot = 0;
-                for (int i = 0; i < player.inventory.Length; i++)
-                {
-                    if (player.inventory[i].ammo == AmmoID.Arrow)
-                    {
-                        ammoIdToShoot = player.inventory[i].shoot;
-                        break;
-                    }
-                }
-                ammoIdToShoot = ammoIdToShoot == ProjectileID.WoodenArrowFriendly ? ModContent.ProjectileType<CrusoliumArrow>() : ammoIdToShoot;
-                if(ammoIdToShoot != 0)
-                Projectile.NewProjectileDirect(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.Center.DirectionTo(Main.MouseWorld) * 10, ammoIdToShoot, Projectile.damage, Projectile.knockBack, Projectile.owner).netUpdate = true;
+                int ammoIdToShoot = GetProjIDToShoot();
+                if (ammoIdToShoot != 0)
+                    Projectile.NewProjectileDirect(Projectile.GetSource_FromThis(), Projectile.Center - Projectile.rotation.ToRotationVector2() * 4, Projectile.Center.DirectionTo(Main.MouseWorld) * 10, ammoIdToShoot, Projectile.damage, Projectile.knockBack, Projectile.owner).netUpdate = true;
                 //fire arrow here
             }
+
             if (ChargeAmount > MaxCharge)
                 Dust.NewDustPerfect(Projectile.Center, DustID.GreenFairy, Vector2.Zero);//DEBUG DUST
             if (Main.myPlayer == Projectile.owner)
                 Projectile.ai[2] = ( Main.MouseWorld - player.Center).ToRotation();
-            Projectile.rotation = Projectile.ai[2];//multiplayer terribleness
-            Projectile.Center = player.Center + new Vector2(30,0).RotatedBy(Projectile.rotation);
             Projectile.netUpdate = true;
+            Projectile.rotation = Projectile.ai[2];//multiplayer terribleness
+            player.direction = (Projectile.rotation > -MathHelper.PiOver2 && Projectile.rotation < MathHelper.PiOver2) ? 1 : -1;
+            Projectile.Center = player.Center - new Vector2(player.direction * 3,2) + new Vector2(23,0).RotatedBy(Projectile.rotation);
+            player.SetCompositeArmFront(true, ProgressToStretchAmount( 1 - ChargeProgressWithEasing), Projectile.rotation - MathF.PI / 2);
+        }
+        int GetProjIDToShoot()
+        {
+            int ammoIdToShoot = 0;
+            Player player = Main.player[Projectile.owner];
+            for (int i = 0; i < player.inventory.Length; i++)
+            {
+                if (player.inventory[i].ammo == AmmoID.Arrow)
+                {
+                    ammoIdToShoot = player.inventory[i].shoot;
+                    break;
+                }
+            }
+            ammoIdToShoot = ammoIdToShoot == ProjectileID.WoodenArrowFriendly ? ModContent.ProjectileType<CrusoliumArrow>() : ammoIdToShoot;
+            return ammoIdToShoot;
         }
         /// <summary>
         /// </summary>
@@ -81,36 +92,53 @@ namespace Crystals.Content.Foresta.Items.Weapons.Ranged.Crusolium
                 controlPoints[2] * ((-3 * tCubed) + (3 * tSquared)) +
                 controlPoints[3] * tCubed);
         }
-        static Color ColorFunction(float progress) => (progress > 0.8 || progress < 0.2) ? Color.GreenYellow : Color.Lime;
-        static float WidthFunction(float progress) => 2;
-        //TODO: actually make the string layer correctly by not using vanilla's vertex strip system. -Photonic0
-        //Also fix the player not holding it and facing the right direction !!! -Photonic0
+        static Color ColorFunction(float progress) => (progress > 0.8f || progress < 0.2f) ? Color.LimeGreen : Color.YellowGreen;
+        static float WidthFunction(float progress) => 4;
         public override bool PreDraw(ref Color lightColor)
         {
             //UNFINISHED
-            Vector2 offsetToStringTip = new(0, 16);
+            Texture2D texture = TextureAssets.Projectile[ProjectileID.CultistBossLightningOrbArc].Value;
             Vector2[] bezierPoints = new Vector2[4];
-            List<Vector2> listForPoints = new();
-            List<float> listForRotations = new();
+            //List<Vector2> listForPoints = new();
+            //List<float> listForRotations = new();
             float[] bezierXOffsets = new float[] { 0, -1, -1, 0 };
             for (float i = 0; i < 4; i++)
             {
-                float progress = ChargeAmount / MaxCharge;
-                progress = (float) Helpers.EaseFunctions.EaseOutBack(progress);
-                bezierPoints[(int)i] = new Vector2(bezierXOffsets[(int)i] * progress * -10 - 10, i * -11 -16).RotatedBy(Projectile.rotation) + Projectile.Center;
+                bezierPoints[(int)i] = new Vector2(bezierXOffsets[(int)i] * ChargeProgressWithEasing * -10 - 12, i * -11 -16).RotatedBy(Projectile.rotation) + Projectile.Center;
             }
-            float maxPoints = 100f;
+            float maxPoints = 30;
             for (float i = 0; i < maxPoints; i++)//probably doesn't need to be this high but uh idk -Photonic0
             {
-                listForPoints.Add(CubicBezier(bezierPoints, (float)i / maxPoints));
-                listForRotations.Add((CubicBezier(bezierPoints, (float)(i + 0.03f) / maxPoints) - CubicBezier(bezierPoints, (float)(i - 0.03f) / maxPoints)).ToRotation());
+                //listForPoints.Add(CubicBezier(bezierPoints, (float)i / maxPoints));
+                Main.EntitySpriteDraw(texture, CubicBezier(bezierPoints, (float)i / maxPoints) - Main.screenPosition, null, ColorFunction(i/maxPoints), 0, texture.Size() / 2, 0.1f, SpriteEffects.None);
+                //listForRotations.Add((CubicBezier(bezierPoints, (float)(i + 0.1f) / maxPoints) - CubicBezier(bezierPoints, (float)(i - 0.1f) / maxPoints)).ToRotation());
             }
-            VertexStrip bowString = new();
-            bowString.PrepareStrip(listForPoints.ToArray(), listForRotations.ToArray(), ColorFunction, WidthFunction, -Main.screenPosition, includeBacksides: true);
-            bowString.DrawTrail();
-            Texture2D texture = TextureAssets.Projectile[Type].Value;
+            //VertexStrip bowString = new();
+            //bowString.PrepareStrip(listForPoints.ToArray(), listForRotations.ToArray(), ColorFunction, WidthFunction, -Main.screenPosition, includeBacksides: true);
+            //bowString.DrawTrail();
+
+            if (ChargeProgress > 0)
+            {
+                texture = TextureAssets.Projectile[GetProjIDToShoot()].Value;
+                Vector2 extraOffset = new Vector2( 10 * ChargeProgressWithEasing - 8, 0).RotatedBy(Projectile.rotation);
+                Projectile proj = new Projectile();
+                proj.type = GetProjIDToShoot();
+                Color color = proj.GetAlpha(Color.White);//I did this to compensate for arrows that are transparent like jester's and holy, so that they look like the actual projectile when released
+                Main.EntitySpriteDraw(texture, Projectile.Center - Main.screenPosition - extraOffset, null, color, Projectile.rotation + MathF.PI / 2, texture.Size() / 2, 1, SpriteEffects.None);
+            }
+            texture = TextureAssets.Projectile[Type].Value;
             Main.EntitySpriteDraw(texture, Projectile.Center - Main.screenPosition, null, Color.White, Projectile.rotation, texture.Size() / 2, Projectile.scale, SpriteEffects.None, 0);
             return false;
+        }
+        static Player.CompositeArmStretchAmount ProgressToStretchAmount(float progress)
+        {
+            if (progress < 0.25f)
+                return Player.CompositeArmStretchAmount.None;
+            if (progress < 0.5f)
+                return Player.CompositeArmStretchAmount.Quarter;
+            if (progress < 0.75f)
+                return Player.CompositeArmStretchAmount.ThreeQuarters;
+            return Player.CompositeArmStretchAmount.Full;
         }
     }
 }
