384e1b9c5321fa99e6b6c5ef78e80d7e3c9c26d6
DvlUp <tekpokanefer@gmail.com>
2023-07-03 00:06:07 +0200

    Finished Sunny

--- Content/Foresta/Npcs/Enemies/Sunny/Sunny.cs
+++ Content/Foresta/Npcs/Enemies/Sunny/Sunny.cs
@@ -1,11 +1,16 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Crystals.Core.Systems.TrailSystem;
 using Crystals.Helpers;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
 using Terraria;
+using Terraria.Audio;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
 
 namespace Crystals.Content.Foresta.Npcs.Enemies.Sunny
 {
@@ -19,14 +24,18 @@ namespace Crystals.Content.Foresta.Npcs.Enemies.Sunny
 
         public override void SetDefaults()
         {
-            NPC.Size = new Vector2(48 ,48);
+            NPC.Size = new Vector2(30 ,30);
             NPC.damage = 32;
             NPC.lifeMax = 80;
             NPC.aiStyle = -1;
             NPC.noGravity = true;
             NPC.noTileCollide = false;
             NPC.defense = 2;
-            NPC.knockBackResist = 0.6f;
+            NPC.knockBackResist = 0.0f;
+            
+            NPC.HitSound = SoundID.NPCDeath7;
+            NPC.DeathSound = SoundID.NPCDeath2;
+            
         }
 
         enum States
@@ -58,10 +67,15 @@ namespace Crystals.Content.Foresta.Npcs.Enemies.Sunny
             
             NPC.spriteDirection = NPC.direction = Math.Sign(target.Center.X - NPC.Center.X);
 
+            PetalRot += 0.05f * NPC.spriteDirection;
+
+            FaceRot = NPC.AngleTo(target.Center);
+
             Timer++;
             switch (state)
             {
                 case States.Idle:
+                    FaceFrame.Y = 0;
                     Idle();
                     if (Timer >= 180f)
                     {
@@ -71,6 +85,7 @@ namespace Crystals.Content.Foresta.Npcs.Enemies.Sunny
                     }
                     break;
                 case States.HalfCircle:
+                    FaceFrame.Y = 62;
                     if (!charging)
                     {
                         StartCharge(NPC.Center , new Vector2(target.Bottom.X , target.Bottom.Y + target.height * 2) , new Vector2(target.Bottom.X , target.Bottom.Y + target.height*2), new Vector2(target.Center.X + NPC.direction * 150 , target.Center.Y - 125));
@@ -88,11 +103,28 @@ namespace Crystals.Content.Foresta.Npcs.Enemies.Sunny
                     }
                     break;
                 case States.SpinShot:
-                    state = States.Idle;
+                    FaceFrame.Y = 62 * 2;
+                    SpinShot();
+                    if (Timer >= 60f)
+                    {
+                        Timer = 0;
+                        state = States.Idle;
+                        NPC.ai[1] = 0;
+                    }
                     break;
             }
 
             #endregion
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            if (spawnInfo.SpawnTileType == TileID.Sunflower)
+            {
+                return SpawnCondition.OverworldDay.Chance;
+            }
+
+            return 0;
         }
 
         private void Idle()
@@ -104,6 +136,7 @@ namespace Crystals.Content.Foresta.Npcs.Enemies.Sunny
             NPC.velocity = NPC.DirectionTo(new Vector2(target.Center.X , target.Center.Y - 125));
 
             NPC.velocity.Y += MathFunctions.SineWave(2f, 1f, NPC.ai[1] / 15);
+
         }
 
         private bool charging = false;
@@ -131,34 +164,139 @@ namespace Crystals.Content.Foresta.Npcs.Enemies.Sunny
             NPC.TargetClosest();
             var target = Main.player[NPC.target];
             Vector2 dest = new MathFunctions.BezierCurve(MathFunctions.EaseFunctions.EaseInOutBack(NPC.ai[1]) , startPos , midPos1 , midPos2 , endPos).GetPoint();
-            PetalRot += NPC.ai[1];
             float speed = NPC.Distance(dest) * 0.125f;
+            PetalRot += NPC.ai[1] * 0.2f;
             NPC.velocity = NPC.DirectionTo(dest) * speed;
         }
 
         private void SpinShot()
         {
+            NPC.ai[1] += 1f / 60f;
+            PetalRot = +MathHelper.SmoothStep(PetalRot, PetalRot += (NPC.ai[1] * 0.25f) * NPC.spriteDirection,
+                MathFunctions.EaseFunctions.EaseInOutQuad(NPC.ai[1]));
             
+            NPC.TargetClosest();
+            var target = Main.player[NPC.target];
+            
+            NPC.velocity = NPC.DirectionTo(new Vector2(target.Center.X , target.Center.Y - 75));
+            if (Timer % 30 == 0 && Main.netMode != NetmodeID.MultiplayerClient)
+            {
+
+                for (int i = 0; i < 4; i++)
+                {
+                    Dust.NewDustPerfect(PetalPos, DustID.YellowStarDust , Main.rand.NextVector2Circular(4 , 4));
+                }
+                
+                Projectile.NewProjectile(NPC.GetSource_None(), PetalPos,
+                    PetalPos.DirectionTo(target.Center) * 16f,
+                    ModContent.ProjectileType<HostilePetal>(), NPC.damage / 2, 0);
+
+                SoundEngine.PlaySound(SoundID.Item63 with { MaxInstances = 2});
+            }
         }
 
         public float PetalRot;
 
+        public Vector2 PetalPos;
+
+        public Rectangle PetalFrame = new Rectangle(0, 0, 62, 62);
+        
+        public Rectangle FaceFrame = new Rectangle(0, 0, 62, 62);
+
+        public float FaceRot;
+
         public override void PostDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
         {
-            
             Texture2D texture = ModContent.Request<Texture2D>("Crystals/Content/Foresta/Npcs/Enemies/Sunny/SunnyPetals").Value;
-            Rectangle frame = new Rectangle(0, 0, 48, 48);
-            Vector2 drawOrigin = new Vector2(frame.Width * 0.5f, frame.Height * 0.5f);
-            Vector2 drawPos = (NPC.Center - Main.screenPosition) + new Vector2(0f, NPC.gfxOffY + 2);
+            
+            Vector2 drawOrigin = new Vector2(PetalFrame.Width * 0.5f, PetalFrame.Height * 0.5f);
+            Vector2 drawPos = (NPC.Center - Main.screenPosition) + new Vector2(0f, NPC.gfxOffY + 4);
 
+            PetalPos = NPC.Top + new Vector2(0f, NPC.gfxOffY + 4);
+            PetalPos.RotatedBy(PetalRot);
 
             SpriteEffects effects = SpriteEffects.None;
             if (NPC.spriteDirection == -1)
             {
-                effects = SpriteEffects.FlipHorizontally;
+                effects = SpriteEffects.FlipVertically;
             }
             
-            spriteBatch.Draw(texture, drawPos, frame, Color.White, PetalRot, drawOrigin, NPC.scale, effects, 0);
+            spriteBatch.Draw(texture, drawPos, PetalFrame, Lighting.GetColor(PetalPos.ToTileCoordinates()), PetalRot, drawOrigin, NPC.scale - 0.20f, effects, 0);
+         
+            Texture2D textureFace = ModContent.Request<Texture2D>("Crystals/Content/Foresta/Npcs/Enemies/Sunny/SunnyFace").Value;
+
+            spriteBatch.Draw(textureFace, drawPos, FaceFrame, Lighting.GetColor(PetalPos.ToTileCoordinates()), FaceRot + NPC.rotation, drawOrigin, NPC.scale, effects, 0);
         }
+        
+        
+        class HostilePetal : ModProjectile 
+        {
+            
+            public override void SetDefaults()
+            {
+                Projectile.Size = new Vector2(14 , 18);
+                Projectile.ignoreWater = false;
+                Projectile.tileCollide = true;
+                Projectile.hostile = true;
+            }
+
+            public override void AI()
+            {
+                Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+                
+                Projectile.ai[0] += 1f; // Use a timer to wait 15 ticks before applying gravity.
+                if (Projectile.ai[0] >= 15f)
+                {
+                    Projectile.ai[0] = 15f;
+                    Projectile.velocity.Y = Projectile.velocity.Y + 0.1f;
+                }
+                if (Projectile.velocity.Y > 16f)
+                {
+                    Projectile.velocity.Y = 16f;
+                }
+                
+                Lighting.AddLight(Projectile.Center ,.255f, .253f, .98f);
+                
+            }
+
+            public PrimitiveTrail trail = new PrimitiveTrail();
+            public List<Vector2> oldPositions = new List<Vector2>();
+            public override bool PreDraw(ref Color lightColor)
+            {
+                
+                Main.spriteBatch.End();
+                Main.spriteBatch.Begin(SpriteSortMode.Deferred, null , null, null, null, null, Main.GameViewMatrix.ZoomMatrix);
+
+                lightColor = Color.White;
+
+                Color color = new Color(255, 253, 98);
+
+                Vector2 pos = (Projectile.Center).RotatedBy(Projectile.rotation, Projectile.Center);
+
+                oldPositions.Add(pos);
+                while (oldPositions.Count > 30)
+                    oldPositions.RemoveAt(0);
+
+                trail.Draw(color, pos, oldPositions, 1f);
+
+                Main.spriteBatch.End();
+                Main.spriteBatch.Begin(SpriteSortMode.Deferred, null, null, null, null, null, Main.GameViewMatrix.ZoomMatrix);
+                return true;
+            }
+            
+            public override void Kill(int timeLeft)
+            {
+                Vector2 position = Projectile.position;
+                for (int i = 0; i < 5; i++)
+                {
+                    Dust.NewDust(position, Projectile.width, Projectile.height, DustID.YellowStarDust,
+                        Main.rand.NextVector2Circular(2 , 2).X * 2, 
+                        Main.rand.NextVector2Circular(2 , 2).Y * 2, 0,
+                        new Color(255, 255, 255), 1f);
+                }
+            }
+            
+        }
+
     }
 }
