129176244ad64811648b1fae2fcf540a23cdc910
DvlUp <tekpokanefer@gmail.com>
2023-07-22 19:55:13 +0200

    Added Base Pumpkin Man

--- Content/Foresta/Npcs/Friendly/PumpkinMan/PumpkinMan.cs
+++ Content/Foresta/Npcs/Friendly/PumpkinMan/PumpkinMan.cs
@@ -1,51 +1,55 @@
 using Terraria;
+using Terraria.GameContent;
+using Terraria.GameContent.Personalities;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Crystals.Content.Foresta.Npcs.Friendly.PumpkinMan
 {
-    /*public class PumpkinMan : ModNPC
+    public class PumpkinMan : ModNPC
     {
+	    
+	    private static Profiles.StackedNPCProfile NPCProfile;
+	    
+	    private static int ShimmerHeadIndex;
         
         public override void SetStaticDefaults() {
-			Main.npcFrameCount[Type] = 25; // The total amount of frames the NPC has
+			Main.npcFrameCount[Type] = 25; 
 
-			NPCID.Sets.ExtraFramesCount[Type] = 9; // Generally for Town NPCs, but this is how the NPC does extra things such as sitting in a chair and talking to other NPCs. This is the remaining frames after the walking frames.
-			NPCID.Sets.AttackFrameCount[Type] = 4; // The amount of frames in the attacking animation.
-			NPCID.Sets.DangerDetectRange[Type] = 700; // The amount of pixels away from the center of the NPC that it tries to attack enemies.
-			NPCID.Sets.AttackType[Type] = 0; // The type of attack the Town NPC performs. 0 = throwing, 1 = shooting, 2 = magic, 3 = melee
-			NPCID.Sets.AttackTime[Type] = 90; // The amount of time it takes for the NPC's attack animation to be over once it starts.
-			NPCID.Sets.AttackAverageChance[Type] = 30; // The denominator for the chance for a Town NPC to attack. Lower numbers make the Town NPC appear more aggressive.
-			NPCID.Sets.HatOffsetY[Type] = 4; // For when a party is active, the party hat spawns at a Y offset.
-			NPCID.Sets.ShimmerTownTransform[NPC.type] = true; // This set says that the Town NPC has a Shimmered form. Otherwise, the Town NPC will become transparent when touching Shimmer like other enemies.
-
-			NPCID.Sets.ShimmerTownTransform[Type] = true; // Allows for this NPC to have a different texture after touching the Shimmer liquid.
+			NPCID.Sets.ExtraFramesCount[Type] = 9; 
+			NPCID.Sets.AttackFrameCount[Type] = 4; 
+			NPCID.Sets.DangerDetectRange[Type] = 500; 
+			NPCID.Sets.AttackType[Type] = 2; 
+			NPCID.Sets.AttackTime[Type] = 90; 
+			NPCID.Sets.AttackAverageChance[Type] = 15; 
+			NPCID.Sets.HatOffsetY[Type] = 4; 
+			NPCID.Sets.ShimmerTownTransform[NPC.type] = true; 
+			NPCID.Sets.ShimmerTownTransform[Type] = true;
 
 			// Influences how the NPC looks in the Bestiary
 			NPCID.Sets.NPCBestiaryDrawModifiers drawModifiers = new NPCID.Sets.NPCBestiaryDrawModifiers(0) {
-				Velocity = 1f, // Draws the NPC in the bestiary as if its walking +1 tiles in the x direction
-				Direction = 1 // -1 is left and 1 is right. NPCs are drawn facing the left by default but ExamplePerson will be drawn facing the right
-				// Rotation = MathHelper.ToRadians(180) // You can also change the rotation of an NPC. Rotation is measured in radians
-				// If you want to see an example of manually modifying these when the NPC is drawn, see PreDraw
+				Velocity = 1f,
+				Direction = 1 
 			};
 
 			NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, drawModifiers);
 
-			// Set Example Person's biome and neighbor preferences with the NPCHappiness hook. You can add happiness text and remarks with localization (See an example in ExampleMod/Localization/en-US.lang).
-			// NOTE: The following code uses chaining - a style that works due to the fact that the SetXAffection methods return the same NPCHappiness instance they're called on.
 			NPC.Happiness
-				.SetBiomeAffection<ForestBiome>(AffectionLevel.Like) // Example Person prefers the forest.
-				.SetBiomeAffection<SnowBiome>(AffectionLevel.Dislike) // Example Person dislikes the snow.
-				.SetBiomeAffection<ExampleSurfaceBiome>(AffectionLevel.Love) // Example Person likes the Example Surface Biome
-				.SetNPCAffection(NPCID.Dryad, AffectionLevel.Love) // Loves living near the dryad.
-				.SetNPCAffection(NPCID.Guide, AffectionLevel.Like) // Likes living near the guide.
-				.SetNPCAffection(NPCID.Merchant, AffectionLevel.Dislike) // Dislikes living near the merchant.
-				.SetNPCAffection(NPCID.Demolitionist, AffectionLevel.Hate) // Hates living near the demolitionist.
-			; // < Mind the semicolon!
-
-			// This creates a "profile" for ExamplePerson, which allows for different textures during a party and/or while the NPC is shimmered.
+				.SetBiomeAffection<HallowBiome>(AffectionLevel.Hate) 
+				.SetBiomeAffection<DesertBiome>(AffectionLevel.Dislike) 
+				.SetBiomeAffection<ForestBiome>(AffectionLevel.Like) 
+				.SetBiomeAffection<JungleBiome>(AffectionLevel.Love)
+				.SetNPCAffection(NPCID.Princess, AffectionLevel.Hate) 
+				.SetNPCAffection(NPCID.Dryad, AffectionLevel.Dislike) 
+				.SetNPCAffection(NPCID.WitchDoctor, AffectionLevel.Like) 
+				.SetNPCAffection(NPCID.Wizard, AffectionLevel.Like)
+				.SetNPCAffection(NPCID.Clothier, AffectionLevel.Like)
+			;
+			
+			
 			NPCProfile = new Profiles.StackedNPCProfile(
-				new Profiles.DefaultNPCProfile(Texture, NPCHeadLoader.GetHeadSlot(HeadTexture), Texture + "_Party"),
-				new Profiles.DefaultNPCProfile(Texture + "_Shimmer", ShimmerHeadIndex, Texture + "_Shimmer_Party")
+				new Profiles.DefaultNPCProfile(Texture, NPCHeadLoader.GetHeadSlot(HeadTexture)),
+				new Profiles.DefaultNPCProfile(Texture + "_Shimmer", ShimmerHeadIndex)
 			);
 		}
 
@@ -64,6 +68,10 @@ namespace Crystals.Content.Foresta.Npcs.Friendly.PumpkinMan
 
 			AnimationType = NPCID.Guide;
 		}
+		
+		public override ITownNPCProfile TownNPCProfile() {
+			return NPCProfile;
+		}
         
-    }*/
+    }
 }
