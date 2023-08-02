7002748d77c456be72037174ab62315781368f9c
Laranjoo <laranjo686@gmail.com>
2023-08-02 13:26:59 -0300

    Added stuff

---
+++ Content/Foresta/Items/EnchantedTimber.cs
@@ -0,0 +1,24 @@
+using Crystals.Helpers;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Crystals.Content.Foresta.Items
+{
+    public class EnchantedTimber : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
+        }
+        
+        public override void SetDefaults()
+        {
+            Item.maxStack = 999;
+            Item.width = 32;
+            Item.height = 32;
+            Item.rare = ItemRarityID.Purple;
+            Item.material = true;
+        }
+    }
+}
---
+++ Content/Foresta/Items/Tools/EnchantedTimberItems/EnchantedTimberAxe.cs
@@ -0,0 +1,33 @@
+using Crystals.Helpers;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Crystals.Content.Foresta.Items
+{
+    public class EnchantedTimberAxe : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+        
+        public override void SetDefaults()
+        {
+            Item.damage = 8;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 34;
+            Item.height = 32;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 4;
+            Item.value = ValueHelper.GetCoinValue(0, 0, 0, 25);
+            Item.rare = ItemRarityID.Blue;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true; // Automatically re-swing/re-use this item after its swinging animation is over.
+
+            Item.axe = 12; // How much axe power the weapon has, note that the axe power displayed in-game is this value multiplied by 5
+        }
+    }
+}
