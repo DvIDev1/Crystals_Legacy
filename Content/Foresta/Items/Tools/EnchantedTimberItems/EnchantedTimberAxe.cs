a38f6a2a65b6abca3a1c77f2f943ef4e1cb3a07f
Laranjoo <laranjo686@gmail.com>
2023-08-02 13:51:18 -0300

    Axe fix

--- Content/Foresta/Items/Tools/EnchantedTimberItems/EnchantedTimberAxe.cs
+++ Content/Foresta/Items/Tools/EnchantedTimberItems/EnchantedTimberAxe.cs
@@ -3,7 +3,7 @@ using Terraria.GameContent.Creative;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace Crystals.Content.Foresta.Items
+namespace Crystals.Content.Foresta.Items.Tools.EnchantedTimberItems
 {
     public class EnchantedTimberAxe : ModItem
     {
