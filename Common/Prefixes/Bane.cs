8d914d8811dd7e1723c9ed4b0b7e85b9ab09e3f9
DvlUp <tekpokanefer@gmail.com>
2023-08-05 01:38:41 +0200

    Works

---
+++ Common/Prefixes/Bane.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Crystals.Common.UI;
+
+namespace Crystals.Common.Prefixes
+{
+    public class Bane : ModPrefix
+    {
+
+        public override PrefixCategory Category => PrefixCategory.AnyWeapon;
+
+        /*public override float RollChance(Item item)
+        {
+            return 0;
+        }*/
+
+        public override bool CanRoll(Item item)
+        {
+            return true;
+        }
+
+
+        public override void SetStats(ref float damageMult, ref float knockbackMult, ref float useTimeMult, ref float scaleMult, ref float shootSpeedMult, ref float manaMult, ref int critBonus)
+        {
+            damageMult *= 1f + 1f;
+            critBonus -= 80;
+            useTimeMult -= 0.80f;
+            knockbackMult *= 1f - 0.80f;
+        }
+
+        public override void ModifyValue(ref float valueMult)
+        {
+            valueMult *= 1f - 1f;
+        }
+
+
+    }
+}
