845484c6b9bce7150337da6fcae5eb36b7e8a9a8
TheLeanMonster <unknown.user2005@yahoo.com>
2023-08-14 17:06:27 -0700

    removed temporarely the cursed effect system

--- Core/Systems/ParticleSystem/CursedItemParticleSys.cs
+++ Core/Systems/ParticleSystem/CursedItemParticleSys.cs
@@ -16,36 +16,6 @@ namespace Crystals.Core.Systems.ParticleSystem
 {
     internal class CursedItemParticleSys : GlobalItem
     {
-        /*
-        public bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale,Item item)
-        {
-            if (Main.netMode != NetmodeID.Server && item.prefix == ModContent.PrefixType<Cursed>() || item.prefix == ModContent.PrefixType<Overwhelming>() || item.prefix == ModContent.PrefixType<Bane>() || item.prefix == ModContent.PrefixType<Devastating>() || item.prefix == ModContent.PrefixType<Hexed>())
-            {
-
-                for (int i = 0; i < 50; i++)
-                {
-                    Vector2 speed = Utils.RandomVector2(Main.rand, -1f, 1f);
-                    Dust dust = Dust.NewDustPerfect(position, DustID.PurpleTorch, speed * 2);
-                    dust.noGravity = true;
-                    Main.NewText("cursed");
-                }
-            }
-            return false;
-        }
-        */
-        public override void PostDrawInInventory(Item item, SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
-        {
-
-           if (Main.netMode != NetmodeID.Server && item.prefix == ModContent.PrefixType<Cursed>() || item.prefix == ModContent.PrefixType<Overwhelming>() || item.prefix == ModContent.PrefixType<Bane>() || item.prefix == ModContent.PrefixType<Devastating>() || item.prefix == ModContent.PrefixType<Hexed>())
-            {
-                
-                for (int i = 0; i < 50; i++)
-                {
-                    Vector2 speed = Utils.RandomVector2(Main.rand, -1f, 1f);
-                    Dust dust = Dust.NewDustPerfect(frame.Center(), DustID.PurpleTorch, speed * 2);
-                    dust.noGravity = true;
-                }
-            }
-        }
+        //leave this here,its for the cursed effect in inventory
     }
 }
