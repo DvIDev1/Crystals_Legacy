954665e57edade510399cf3ff191a1670acbd349
DvlUp <tekpokanefer@gmail.com>
2023-08-29 21:33:41 +0200

    Updating Cruso Stuff

--- Content/Foresta/Items/Weapons/Melee/Crusolium/CrusoSpear.cs
+++ Content/Foresta/Items/Weapons/Melee/Crusolium/CrusoSpear.cs
@@ -22,13 +22,13 @@ namespace Crystals.Content.Foresta.Items.Weapons.Melee.Crusolium
 
 			// Use Properties
 			Item.useStyle = ItemUseStyleID.Shoot; // How you use the item (swinging, holding out, etc.)
-			Item.useAnimation = 30; // The length of the item's use animation in ticks (60 ticks == 1 second.)
-			Item.useTime = 30; // The length of the item's use time in ticks (60 ticks == 1 second.)
-			Item.UseSound = SoundID.Item1; // The sound that this item plays when used.
+			Item.useAnimation = 40; // The length of the item's use animation in ticks (60 ticks == 1 second.)
+			Item.useTime = 40; // The length of the item's use time in ticks (60 ticks == 1 second.)
+			Item.UseSound = SoundID.DD2_GhastlyGlaivePierce; // The sound that this item plays when used.
 			Item.autoReuse = true; // Allows the player to hold click to automatically use the item again. Most spears don't autoReuse, but it's possible when used in conjunction with CanUseItem()
 
 			// Weapon Properties
-			Item.damage = 75;
+			Item.damage = 19;
 			Item.knockBack = 6.5f;
 			Item.noUseGraphic = true; // When true, the item's sprite will not be visible while the item is in use. This is true because the spear projectile is what's shown so we do not want to show the spear sprite as well.
 			Item.DamageType = DamageClass.Melee;
