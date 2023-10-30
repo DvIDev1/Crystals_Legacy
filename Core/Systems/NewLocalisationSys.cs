7d9c85beebe03a74662e49b69be116e88d398647
StaticCerebrum <unknown.user2005@yahoo.com>
2023-10-30 09:29:54 -0700

    localisation roundabout

---
+++ Core/Systems/NewLocalisationSys.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace Crystals.Core.Systems
+{
+    internal class NewLocalisationSys : ModSystem
+    {
+        public override void PostSetupContent()
+        {
+            MethodInfo refreshInfo = typeof(LocalizationLoader).GetMethod("UpdateLocalizationFilesForMod", BindingFlags.NonPublic | BindingFlags.Static, new Type[] { typeof(Mod), typeof(string), typeof(GameCulture) });
+            refreshInfo.Invoke(null, new object[] { Mod, null, Language.ActiveCulture });
+
+        }
+    }
+
+    internal static class NewLocalisationRoundabout
+    {
+        public static void SetDefault(this LocalizedText text, string value)
+        {
+            PropertyInfo valueProp = typeof(LocalizedText).GetProperty("Value", BindingFlags.Public | BindingFlags.Instance);
+
+            LanguageManager.Instance.GetOrRegister(text.Key, () => value);
+            valueProp.SetValue(text, value);
+        }
+    }
+}
